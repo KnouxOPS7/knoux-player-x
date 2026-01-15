9be47979b3f0ce4675f8f183dd6aecf72bbcbbcd
knoux <knouxbook@gmail.com>
2026-01-15 15:13:33 +0400

    a

--- .webpack/main/index.js
+++ .webpack/main/index.js
@@ -2,6 +2,70 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "./desktop/main/main.ts"
+/*!******************************!*\
+  !*** ./desktop/main/main.ts ***!
+  \******************************/
+(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+const electron_1 = __webpack_require__(/*! electron */ "electron");
+const fs_1 = __importDefault(__webpack_require__(/*! fs */ "fs"));
+const path_1 = __importDefault(__webpack_require__(/*! path */ "path"));
+let mainWindow = null;
+function resolvePreload(preloadPath) {
+    try {
+        if (fs_1.default.existsSync(preloadPath))
+            return preloadPath;
+        const alt1 = path_1.default.join(process.cwd(), '.webpack', 'x64', 'renderer', 'main_window', 'preload.js');
+        if (fs_1.default.existsSync(alt1))
+            return alt1;
+        const alt2 = path_1.default.join(process.cwd(), '.webpack', 'renderer', 'main_window', 'preload.js');
+        if (fs_1.default.existsSync(alt2))
+            return alt2;
+        return preloadPath;
+    }
+    catch (_a) {
+        return preloadPath;
+    }
+}
+function createWindow() {
+    mainWindow = new electron_1.BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            preload: resolvePreload('C:\\Users\\Aisha\\Downloads\\knox-player-x™\\.webpack\\renderer\\main_window\\preload.js'),
+        },
+    });
+    mainWindow.loadURL('http://localhost:3000/main_window/index.html');
+    if (true) {
+        mainWindow.webContents.openDevTools();
+    }
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+electron_1.app.whenReady().then(createWindow);
+electron_1.app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        electron_1.app.quit();
+    }
+});
+electron_1.app.on('activate', () => {
+    if (electron_1.BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
+
+
+/***/ },
+
 /***/ "electron"
 /*!***************************!*\
   !*** external "electron" ***!
@@ -9,6 +73,26 @@
 (module) {
 
 module.exports = require("electron");
+
+/***/ },
+
+/***/ "fs"
+/*!*********************!*\
+  !*** external "fs" ***!
+  \*********************/
+(module) {
+
+module.exports = require("fs");
+
+/***/ },
+
+/***/ "path"
+/*!***********************!*\
+  !*** external "path" ***!
+  \***********************/
+(module) {
+
+module.exports = require("path");
 
 /***/ }
 
@@ -38,57 +122,20 @@ module.exports = require("electron");
 /******/ 		};
 /******/ 	
 /******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 /******/ 	
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
 /******/ 	
 /************************************************************************/
-var __webpack_exports__ = {};
-// This entry needs to be wrapped in an IIFE because it needs to be isolated against other modules in the chunk.
-(() => {
-var exports = __webpack_exports__;
-/*!******************************!*\
-  !*** ./desktop/main/main.ts ***!
-  \******************************/
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-const electron_1 = __webpack_require__(/*! electron */ "electron");
-let mainWindow = null;
-function createWindow() {
-    mainWindow = new electron_1.BrowserWindow({
-        width: 1200,
-        height: 800,
-        webPreferences: {
-            nodeIntegration: false,
-            contextIsolation: true,
-            preload: 'C:\\Users\\Aisha\\Downloads\\knox-player-x™\\.webpack\\renderer\\main_window\\preload.js',
-        },
-    });
-    mainWindow.loadURL('http://localhost:3000/main_window/index.html');
-    if (true) {
-        mainWindow.webContents.openDevTools();
-    }
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
-}
-electron_1.app.whenReady().then(createWindow);
-electron_1.app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        electron_1.app.quit();
-    }
-});
-electron_1.app.on('activate', () => {
-    if (electron_1.BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
-});
-
-})();
-
-module.exports = __webpack_exports__;
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __webpack_require__("./desktop/main/main.ts");
+/******/ 	module.exports = __webpack_exports__;
+/******/ 	
 /******/ })()
 ;
 //# sourceMappingURL=index.js.map
